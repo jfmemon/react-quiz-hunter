aba41a442ce05dea47205c8eac5ecd0ea651914d
jfmemon <jannatul15-2497@diu.edu.bd>
2023-04-16 00:21:17 +0600

    correct answer toast show done

--- src/Components/Options/Options.js
+++ src/Components/Options/Options.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Options = ({ option, id }) => {
+    const [active, setActive] = useState(false);
+    const handleBgColor = () => {
+        setActive(!active);
+    }
     return (
         <div>
-            <div className='border border-dark m-2 p-3 rounded' role="button">
+            <div onClick={handleBgColor} style={{backgroundColor: active ? 'gray' : 'white'}} className='border border-dark m-2 p-3 rounded' role="button">
                 {option}
             </div>
         </div>
--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Options from '../Options/Options';
+import { Toast } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 const Question = ({ quiz }) => {
+    console.log(quiz);
+    const [show, setShow] = useState(false);
     const options = quiz.options;
     const id = quiz.id;
+    const setCorrectAnswer = () => {
+        setShow(!show);
+    }
     return (
         <div className='question-container border m-4 p-3 bg-info bg-opacity-25'>
+            <Toast onClose={setCorrectAnswer} show={show} animation={false}>
+                <Toast.Header>
+                    <img
+                        src="holder.js/20x20?text=%20"
+                        className="rounded me-2"
+                        alt=""
+                    />
+                    <strong className="me-auto">Correct Answer</strong>
+                </Toast.Header>
+                <Toast.Body>{quiz.correctAnswer}</Toast.Body>
+            </Toast>
             <div>
-                <h5 className='my-3'>{quiz.question}</h5>
+                <div className='d-flex justify-content-between'>
+                    <h5 className='my-3'>{quiz.question}</h5>
+                    <FontAwesomeIcon role='button' onClick={setCorrectAnswer} icon={faEye} />
+                </div>
                 <div>
                     {
                         options.map(option => <Options
